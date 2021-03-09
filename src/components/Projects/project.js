1ec838f24b05e5ca943015745c911d3dcd8cc778
geertza <geertza@gmail.com>
2021-03-09 08:51:57 -0700

    removed mobile toggle on projects

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,42 +1,128 @@
 import React, { Component } from 'react'
-import ProMobile from './proMobile'
-import ProPc from './proPc'
+import info from "./ProjectInfo"
+import BackgroundImage from 'gatsby-background-image'
+import styled from 'styled-components';
+export default class proMobile extends Component {
+    
+    render() {
+        const  getBackground = (id,data) =>{
+                let item =  data.find(item=>item.node.name === (id+'Icon'));
+            console.log('item',item)
+                return(item.node.childrenImageSharp[0].fluid)
+            }
 
-// map out card for each project to be displayed
-export default class project extends Component {
-    constructor() {
-        super();
-        this.state = {
-          width: window.innerWidth,
-        };
-      }
-    //   event listeners to determine if client device is mobile and return different layouts for different devices
-      componentWillMount() {
-        window.addEventListener('resize', this.handleWindowSizeChange);
-      }
+          //   styled components
+          const Projects=styled.article`
+          width: 100vw;
+          flex-wrap: wrap;
+          
+        `
+
+        const ProCard=styled.div`
+          
+        width: 90vw;
+          height: fit-content;
+          margin: 0px 5vw;
+          z-index:3;
+          @media only screen and (min-width: 600px) {
+            margin: 30vh 5vw;
+          }
+        `
       
-      // make sure to remove the listener
-      // when the component is not mounted anymore
-      componentWillUnmount() {
-        window.removeEventListener('resize', this.handleWindowSizeChange);
-      }
-      
-      handleWindowSizeChange = () => {
-        this.setState({ width: window.innerWidth });
-      };
-      
-      render() {
-        const { width } = this.state;
-        const isMobile = width <= 600;
         
-        if (isMobile) {
-            return (
-                <ProMobile data={this.props.data} />
-            );
-          } else {    
+        const Article =styled.h4`  
+            font-size: .7em;
+            lineHeight: 100%;
+            width:100%;
+           background-color: var(--bg);
+           margin-left:5px;
+           margin-top:5px; 
+           float:left;          
+        `
+        const Button =styled.a`
+            width:fit-content;
+            height: fit-content;
+            background-color: var(--bg2);
+            font-size: .7em;
+            text-shadow: none;
+            border-radius: 10px;
+            border-style:solid;
+            border-color:var(--bg3);
+            margin:  0 ;
+
+            padding:4px;
+            color: var(--text);
+            font-weight :800;
+            cursor: pointer;
+            float:right;
+            &:hover{
+                background-color:var(--bg3);
+                color:var(--bg2);
+            }
+        ` 
+        const Image =styled.img`
+        font-size:2em;
+        margin: 0 ;
+        padding: 0;
+    `
+        const Ul = styled.ul`
+        list-style: none;
+        display: flex;
+        @media only screen and (min-width: 600px) {
+          transform: translateX(20vw) translateY(-5vh);
+        }
+        ` 
         return (
-           <ProPc data={this.props.data}/>
+            <Projects id='projects'>
+            {info.map(i => (
+              // getBackground(i.id,this.props.data),
+              <ProCard id={i.id} key={i.key} > 
+                <h3 style={{
+                    color:'var(--highlight)',
+                    textShadow:'2px 1px 2px silver',
+                    backgroundColor:'var(--bg1)',
+                    textAlign:'center',
+                    top:'0',
+                    margin:'0',
+                    backgroundColor:'var(--bg3)',
+                    }}>
+                        {i.title}
+                </h3>
+                <div style={{display:'flex'}} >
+              <div>
+              <BackgroundImage
+              className='ProImage'
+                fluid={getBackground(i.id,this.props.data.tv.edges)}
+                   />  
+                 <Ul  >
+                        {i.lang.map(function (image){
+                            return(
+                              <li  key={image}>
+                                <Image 
+                                src={require(`../../images/${image}.png`)}  alt='Cool Skills' 
+                                 />
+                                </li>
+                            )
+                        })
+                        }
+                    </Ul>   
+                    </div>
+                    <div>
+                      <Article >
+                          {i.p1}
+                          </Article>
+                          <Article style={{margin:'0 0 10px 2vw'}}>
+                          {i.p2}
+                      </Article>
+                <div style={{display:'flex',position:'relative' ,bottom:'0vh',float:'right'}} >
+                    <Button  href={i.git}>See The Code</Button>
+                    <Button  href={i.demo}>See It In Action</Button>
+                    </div> 
+              </div>
+              </div>
+               </ProCard>
+        ))}
+            </Projects>
         )
     }
-}
 }
