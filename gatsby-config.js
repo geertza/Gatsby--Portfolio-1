ba354267f96a54a576e302ff2150d26277250fff
geertza <geertza@gmail.com>
2021-03-12 11:08:03 -0700

    testing manifest error

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // 'gatsby-plugin-favicon',
     `gatsby-plugin-purgecss`,
     {
       resolve: `gatsby-plugin-manifest`,
